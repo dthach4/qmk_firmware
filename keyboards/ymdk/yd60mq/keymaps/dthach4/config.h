9a36b387aebdb48d10b92847e018bdbc6d637eac
Dany Thach <d.thach4@gmail.com>
2025-05-26 14:47:13 +0200

    Added dthach4 keymap in ymdk/yd60mq

---
+++ keyboards/ymdk/yd60mq/keymaps/dthach4/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Mouse movement
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_DELAY                          5
+#define MOUSEKEY_INTERVAL                      10
+#define MOUSEKEY_MOVE_DELTA                    16
+#define MOUSEKEY_INITIAL_SPEED                100
+#define MOUSEKEY_BASE_SPEED                  5000
+#define MOUSEKEY_DECELERATED_SPEED            400
+#define MOUSEKEY_ACCELERATED_SPEED           3000
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS       16
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS          32
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS   48
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS    8
